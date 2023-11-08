0ff0490e17e63550fbc8aa752bee10d62373890e
jennifer-ledezma <jennylv17@gmail.com>
2023-11-07 19:33:45 -0600

    First steps with Firebase instead of fakeApi

--- src/components/itemListContainer/itemListContainer.jsx
+++ src/components/itemListContainer/itemListContainer.jsx
@@ -2,45 +2,56 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Spinner from 'react-bootstrap/Spinner'
-import { useParams } from 'react-router-dom'
+// import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import Item from '../item/item'
+import { db } from '../../firebase/client'
+import { collection, getDocs } from 'firebase/firestore'
 
 const ItemListContainer = ({ greeting }) => {
     const [products, setProducts] = useState([])
     const [title, setTitle] = useState('All products')
-    const { categoryName } = useParams()
+    // const { categoryName } = useParams()
+    // const url = categoryName ? `https://fakestoreapi.com/products/category/${categoryName}` : 'https://fakestoreapi.com/products/'
     const [loading, setLoading] = useState(true) // Add loading state
-    const url = categoryName ? `https://fakestoreapi.com/products/category/${categoryName}` : 'https://fakestoreapi.com/products/'
+    
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(url)
+        // const fetchData = async () => {
+        //     try {
+        //         const response = await fetch(url)
 
-                if (!response.ok) {
-                    throw new Error('Network response was not ok')
-                }
+        //         if (!response.ok) {
+        //             throw new Error('Network response was not ok')
+        //         }
 
-                const json = await response.json()
-                setProducts(json)
-                categoryName ? setTitle(categoryName) : setTitle('All products')
-            } catch (error) {
-                console.error(error)
-            } finally {
-                setLoading(false)
-            }
-        }
+        //         const json = await response.json()
+        //         setProducts(json)
+        //         categoryName ? setTitle(categoryName) : setTitle('All products')
+        //     } catch (error) {
+        //         console.error(error)
+        //     } finally {
+        //         setLoading(false)
+        //     }
+        // }
+
+        const productsRef = collection(db, "products")
+
+        getDocs(productsRef)
+        .then((snapshot) => {    
+            setProducts(snapshot.docs.map(doc => ({ id: doc.id, ...doc.data()})))
+            setTitle('All products')
+        })
+        .catch(e => console.error(e))
+        .finally(setLoading(false))
 
         const timeoutId = setTimeout(() => {
             console.error('Request timed out')
             setLoading(false)
         }, 10000)
 
-        fetchData()
-
         return () => clearTimeout(timeoutId)
-    }, [categoryName])
+    }, [])
 
     return (
         <Container>
---
+++ src/firebase/client.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuw7NhymX1LfPbkoet_dVFG7Yja9us7nQ",
+  authDomain: "skinglow-ae998.firebaseapp.com",
+  projectId: "skinglow-ae998",
+  storageBucket: "skinglow-ae998.appspot.com",
+  messagingSenderId: "663686309231",
+  appId: "1:663686309231:web:2493dc325fd8c07170749a"
+};
+
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
